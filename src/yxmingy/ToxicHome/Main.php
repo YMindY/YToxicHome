7acc66b3dc35ea99fdd2e0ef2fb2262ed2a8f57f
YxMingY <526817087@qq.com>
2018-12-11 22:10:13 +0800

    真言附加

--- src/yxmingy/ToxicHome/Main.php
+++ src/yxmingy/ToxicHome/Main.php
@@ -1,4 +1,13 @@
 <?php
+/*
+  Date: 2018.12.11
+  Author: xMing
+  Editor: Quoda
+  Poem:
+    手持两把锟斤拷，口中疾呼烫烫烫。
+    脚踏千朵屯屯屯，笑看万物锘锘锘。
+  Mantra: 高内聚，低耦合。
+*/
 namespace yxmingy\ToxicHome;
 class Main extends starter\Starter
 {
