20c767cfedfe05042ee0ed24fd533415559adea0
YxMingY <526817087@qq.com>
2018-12-30 01:10:55 +0800

    填方块(这操作有毒)

--- src/yxmingy/ToxicHome/store/BuyCommand.php
+++ src/yxmingy/ToxicHome/store/BuyCommand.php
@@ -4,17 +4,39 @@ use yxmingy\ToxicHome\starter\CommandExecutor;
 use yxmingy\ToxicHome\generator\GeneratorManager;
 use pocketmine\Server;
 use pocketmine\command\CommandSender;
+use onebone\economyapi\EconomyAPI;
+use pocketmine\math\Vector3;
+use pocketmine\level\Position;
+use pocketmine\block\Block;
 class BuyCommand implements CommandExecutor
 {
   public function handleCommand(array $args,CommandSender $sender):bool
   {
     if(!isset($args[0])) return $this->return($sender);
     if(!in_array($args[0],array("0","1","2","3"),true)) return $this->return($sender);
-    if(Server::getInstance()->isLevelGenerated("ythome_".$sender->getName())){
+    $name = $sender->getName();
+    if(Server::getInstance()->isLevelGenerated("ythome_".$name)){
       $sender->sendMessage("你都有家了还凑什么热闹");
       return true;
     }
-    if(GeneratorManager::generateLevel($sender->getName(),$args[0])){
+    $eapi = EconomyAPI::getInstance();
+    $price = HomeStore::getConfig()[$args[0]."级世界售价"];
+    if($eapi->MyMoney($name) < $price)
+    {
+      $sender->sendMessage("穷鬼，等攒够".$price."再来吧!");
+      return true;
+    }
+    if(GeneratorManager::generateLevel($name,$args[0])){
+      $level = Server::getInstance()->getLevelbyName("ythome_".$name);
+      $level->setSpawnLocation(new Vector3(0,1,0));
+      $grass = Block::get(Block::GRASS);
+      $y = 0;
+      $size = array(
+      "0"=>9,
+      "1"=>64,
+      "2"=>128,
+      "3"=>256
+      );
       $sender->sendMessage("购买成功!");
     }
     return true;
--- src/yxmingy/ToxicHome/store/HomeStore.php
+++ src/yxmingy/ToxicHome/store/HomeStore.php
@@ -8,7 +8,7 @@ class HomeStore
   private static $conf;
   public static function init():void
   {
-    self::$conf = new Config(Main::getInstance()->getDataFolder()."/store.yml",Config::YAML,array("初级世界售价"=>2000,"一级世界售价"=>5000,"二级世界售价"=>10000,"三级世界售价"=>20000));
+    self::$conf = new Config(Main::getInstance()->getDataFolder()."/store.yml",Config::YAML,array("0级世界售价"=>2000,"1级世界售价"=>5000,"2级世界售价"=>10000,"3级世界售价"=>20000));
   }
   public static function getConfig():array
   {
