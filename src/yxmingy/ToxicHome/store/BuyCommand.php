f181a017658b2b605c025e5907fdf881de509017
YxMingY <526817087@qq.com>
2018-12-30 13:11:41 +0800

    emm

--- src/yxmingy/ToxicHome/store/BuyCommand.php
+++ src/yxmingy/ToxicHome/store/BuyCommand.php
@@ -20,23 +20,38 @@ class BuyCommand implements CommandExecutor
       return true;
     }
     $eapi = EconomyAPI::getInstance();
-    $price = HomeStore::getConfig()[$args[0]."级世界售价"];
+    $price = HomeStore::getConfig()[$args[0]."级世界"]["售价"];
+    /*
     if($eapi->MyMoney($name) < $price)
     {
       $sender->sendMessage("穷鬼，等攒够".$price."再来吧!");
       return true;
     }
-    if(GeneratorManager::generateLevel($name,$args[0])){
-      $level = Server::getInstance()->getLevelbyName("ythome_".$name);
+   */ if(GeneratorManager::generateLevel($name,$args[0])){
+      $level = Server::getInstance()->getLevelbyName((string)("ythome_".$name));
+      //var_dump($level);
       $level->setSpawnLocation(new Vector3(0,1,0));
       $grass = Block::get(Block::GRASS);
       $y = 0;
-      $size = array(
-      "0"=>9,
-      "1"=>64,
-      "2"=>128,
-      "3"=>256
-      );
+      $size = HomeStore::getConfig()[$args[0]."级世界"]["大小"];
+      $asize = ($size-1)/2;//anglesize
+      for($fx=-$asize;$fx<=$asize;$fx++)
+      {
+        for($fz=-$asize;$fz<=$asize;$fz++)
+        {
+          if(!$level->isChunkLoaded($fx,$fz))
+          {
+            $level->loadChunk($fx,$fz);
+            if(($chunk=$level->getChunk($fx,$fz,true)) === null)
+            {
+              $sender->sendMessage("出现无法解决的错误!");
+            }
+          }
+          var_dump($level->isChunkLoaded($fx,$fz));
+          //$level->setBlock(new Vector3($fx,0,$fz),clone $grass);
+          $chunk->setBlock($fx & 0xf, 0, $fz & 0xf, $grass->getId(), $grass->getDamage());
+        }
+      }
       $sender->sendMessage("购买成功!");
     }
     return true;
--- src/yxmingy/ToxicHome/store/HomeStore.php
+++ src/yxmingy/ToxicHome/store/HomeStore.php
@@ -8,7 +8,7 @@ class HomeStore
   private static $conf;
   public static function init():void
   {
-    self::$conf = new Config(Main::getInstance()->getDataFolder()."/store.yml",Config::YAML,array("0级世界售价"=>2000,"1级世界售价"=>5000,"2级世界售价"=>10000,"3级世界售价"=>20000));
+    self::$conf = new Config(Main::getInstance()->getDataFolder()."/store.yml",Config::YAML,array("0级世界"=>["售价"=>2000,"大小"=>9],"1级世界"=>["售价"=>5000,"大小"=>63],"2级世界"=>["售价"=>2000,"大小"=>127],"3级世界"=>["售价"=>2000,"大小"=>255]));
   }
   public static function getConfig():array
   {
